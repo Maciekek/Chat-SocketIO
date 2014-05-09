09a8e32637248006d6359ac1188dcf4dea425a75
Maciekek <maciekek@gmail.com>
2014-05-09 16:55:25 +0200

    Wywalenie zbędnego kodu. Aplikacja na etapie możliwości zmiany kanały z defaultowego na kanał: "room1".

--- app.js
+++ app.js
@@ -19,103 +19,11 @@ io.sockets.on("connection", function(socket){
         console.log("Rozpoczynam nadawanie na kanale: " + socket.room);
     });
     socket.on('send msg', function(data){
-
-//      console.log('send msg');
-//      history.unshift(data);
-//      io.sockets.in(socket.room).emit('rec msg', data);
-//      socket.on('change room', function(newRoom){
-//      console.log("zmieniam Pokoj");
-//      socket.leave(socket.room);
-//      socket.join(newRoom);
-//      socket.room = newRoom;
-//      console.log(socket.room);
-//      socket.emit("send msg", "change to rooom2");
         history.unshift(data);
         io.sockets.in(socket.room).emit('rec msg', data + " | pokój: "+ socket.room + " |");
    });
    socket.emit('history', history);
 });
-
-
-
-
-//io.sockets.on('connection', function (socket) {
-//    socket.on('send msg', function (data) {
-//        history.unshift(data);
-//        io.sockets.emit('rec msg', data);
-//    });
-//    socket.emit('history', history);
-//});
-
-//var room1 = io
-//    .of('/room1')
-//    .on('connection', function (socket) {
-//        console.log("soccet connection on room1");
-//        socket.on('send msg', function (data) {
-//            history.unshift(data);
-//
-//            socket.emit('rec msg', data);
-//            socket.broadcast.emit('rec msg', data);
-//
-//        });
-//        socket.emit('history', history);
-//        socket.emit('rooms', rooms);
-//
-//        socket.on("new room", function(data){
-//            console.log("Tworze nowy pokój: " + data);
-//            rooms.push(data);
-//            console.log("Istniejace pokoje: " + rooms);
-//            socket.emit('show rooms', rooms);
-//            socket.broadcast.emit('show rooms', rooms);
-//        });
-//    });
-//var room2 = io
-//    .of('/room2')
-//    .on('connection', function (socket) {
-//        console.dir(socket);
-//        socket.on('send msg', function (data) {
-//            history.unshift(data);
-//            console.log(data);
-//            socket.emit('rec msg', data);
-//            socket.broadcast.emit('rec msg', data);
-//
-//        });
-//        socket.emit('history', history);
-//        socket.emit('rooms', rooms);
-//
-//        socket.on("new room", function(data){
-//            console.log("Tworze nowy pokój: " + data);
-//            rooms.push(data);
-//            console.log("Istniejace pokoje: " + rooms);
-//            socket.emit('show rooms', rooms);
-//            socket.broadcast.emit('show rooms', rooms);
-//        });
-//    });
-//        var room3 = io
-//            .of('/room3')
-//            .on('connection', function (socket) {
-//                console.log("soccet connection on room3");
-//                socket.on('send msg', function (data) {
-//                    history.unshift(data);
-//
-//                    socket.emit('rec msg', data);
-//                    socket.broadcast.emit('rec msg', data);
-//
-//                });
-//                socket.emit('history', history);
-//                socket.emit('rooms', rooms);
-//
-//                socket.on("new room", function(data){
-//                    console.log("Tworze nowy pokój: " + data);
-//                    rooms.push(data);
-//                    console.log("Istniejace pokoje: " + rooms);
-//                    socket.emit('show rooms', rooms);
-//                    socket.broadcast.emit('show rooms', rooms);
-//                });
-//            });
-
-
-
 
 httpServer.listen(3000, function () {
     console.log('Serwer HTTP działa na pocie 3000');
