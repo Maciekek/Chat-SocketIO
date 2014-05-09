b11a96d5fd5860d3c5d516ab293c33744d7a55cd
Maciekek <maciekek@gmail.com>
2014-05-09 16:51:22 +0200

    Init commit,

---
+++ public/js/main.js
@@ -0,0 +1,79 @@
+var app = angular.module('czatApka', []);
+
+app.factory('socket', function () {
+    var socket = io.connect('http://' + location.host + "/");
+    return socket;
+});
+
+app.controller('chatCtrlr', ['$scope', 'socket',
+    function ($scope, socket) {
+        var tagsToReplace = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;'
+        };
+        var replaceTag = function (tag) {
+            return tagsToReplace[tag] || tag;
+        };
+        var safe_tags_replace = function (str) {
+            return str.replace(/[&<>]/g, replaceTag);
+        };
+        var prepareMessageWithLogin = function(msg, login){
+            return login + ": " +  safe_tags_replace($scope.msg.text.substring(0, 20));
+        };
+        $scope.msgs = [];
+        $scope.rooms = [];
+
+        $scope.connected = false;
+        $scope.newRoom = function() {
+            console.log("zmieniam pokoj");
+            socket.emit("change room", 'room2');
+
+        };
+        $scope.joinRoom = function()   {
+            socket.emit("start");
+        };
+        $scope.sendMsg = function () {
+            if ($scope.msg && $scope.msg.text) {
+                socket.emit('send msg', prepareMessageWithLogin($scope.msg.text, $scope.msg.login));
+                $scope.msg.text = '';
+            }
+            console.log("Poszło na soccet: " + socket.name);
+        };
+
+        $scope.chooseRoom = function(room){
+          socket.leave(socket.room);
+          socket.join(room);
+
+          $scope.msgs = "";
+        };
+
+        socket.on('connect', function () {
+            $scope.connected = true;
+            $scope.$digest();
+        });
+
+        socket.on('history', function (data) {
+            console.log("history " + data);
+            $scope.msgs = data;
+            $scope.$digest();
+        });
+
+        socket.on('rooms', function (data){
+            console.log("utworzone pokoje: "+ data);
+            $scope.rooms = data;
+            $scope.$digest();
+        });
+
+        socket.on('rec msg', function (data) {
+            console.log("rec msg " + data);
+            $scope.msgs.unshift(data);
+            $scope.$digest();
+        });
+        socket.on('show rooms', function(data){
+            console.log("Wyswietlam pokoje:" + data);
+            $scope.rooms = data;
+            $scope.$digest();
+        });
+    }
+]);
